8fba1609d3a329de1f40f79f138b523419f65ef5
Leeto <maxfox590@gmail.com>
2022-05-23 18:59:59 +0300

    settings modified

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn name.wsgi
+web: gunicorn name.wsg
--- name/settings.py
+++ name/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -124,5 +125,3 @@ LOGIN_REDIRECT_URL = 'todolist'
 LOGIN_URL = 'login'
 
 django_heroku.settings(locals())
-
-
